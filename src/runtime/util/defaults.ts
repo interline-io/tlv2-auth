a249988a54daec59ad5b312d2abe674ca15f083a
Ian Rees <ian@ianrees.net>
2026-04-10 03:31:58 -0700

    Address comments

--- src/runtime/util/defaults.ts
+++ src/runtime/util/defaults.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_AUTH_PREFIX = '/auth'
 export const DEFAULT_PROXY_PREFIX = '/proxy'
 /** Placeholder domain used when auth0 credentials are not provided. */
-export const AUTH0_PLACEHOLDER_DOMAIN = 'disabled'
+export const AUTH0_PLACEHOLDER_DOMAIN = 'disabled.invalid'
